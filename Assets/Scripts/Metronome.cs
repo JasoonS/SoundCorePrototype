9367ebaea6568d2b2728f7c5a9e2a4f18db950ea
Jason Smythe <jason@smythemail.za.net>
2016-05-14 21:25:11 +0200

    Basic sequence editor

---
+++ Assets/Scripts/Metronome.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Metronome : MonoBehaviour {
+
+  public double bpm;
+  public int beatsPerBar;
+  public SoundPlayer soundPlayer;
+  private int currentBeat = 0;
+  private int prevBeat = 0;
+  private double startTime = -1.0F;
+  private bool running = false;
+  private bool isBeatOne = false;
+
+  public void start() {
+    this.running = true;
+    startTime = AudioSettings.dspTime;
+  }
+
+  void Update() {
+    prevBeat = currentBeat;
+    currentBeat = (int)((AudioSettings.dspTime - startTime) / (60.0F / bpm)) % beatsPerBar;
+
+    isBeatOne = (prevBeat == beatsPerBar - 1 && currentBeat == 0);
+    if (prevBeat != currentBeat) {
+      Debug.Log("HERE YOU CAN EXECUTE ALL THE FUNCTIONS THAT HAPPEN EVERY BEAT.");
+      Debug.Log("it is beat one: " + isBeatOne);
+      soundPlayer.playSound(currentBeat);
+    }
+
+  }
+}
---
+++ Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SoundPlayer : MonoBehaviour {
+
+  public GridDisplay grid;
+  public AudioSource clip1;
+  public AudioSource clip2;
+  public AudioSource clip3;
+  public AudioSource clip4;
+
+  public double frequency = 440;
+  public double gain = 1;
+  public bool manipulateSound = false;
+
+  private double increment;
+  private double phase;
+  private double sampling_frequency = 48000;
+
+  void Start () {
+
+  }
+
+  void Update () {
+
+  }
+
+  public void playSound(int beat) {
+    Debug.Log("Beat - " + beat);
+    if (grid.displayTokens[0, beat].selected) {
+      clip2.Play();
+    }
+    if (grid.displayTokens[1, beat].selected) {
+      Debug.Log("GREEN - " + beat);
+    }
+    if (grid.displayTokens[2, beat].selected) {
+      Debug.Log("YELLOW - " + beat);
+    }
+    if (grid.displayTokens[3, beat].selected) {
+      Debug.Log("BLUE - " + beat);
+    }
+  }
+
+  void OnAudioFilterRead(float[] data, int channels)
+  {
+    if (manipulateSound){
+      // update increment in case frequency has changed
+      increment = frequency * 2 * Math.PI / sampling_frequency;
+      for (var i = 0; i < data.Length; i = i + channels)
+      {
+        phase = phase + increment;
+      // this is where we copy audio data to make them “available” to Unity
+        data[i] = data[i] * (float)(gain*Math.Sin(phase));
+      // if we have stereo, we copy the mono data to each channel
+        if (channels == 2) data[i + 1] = data[i];
+        if (phase > 2 * Math.PI) phase = 0;
+      }
+    }
+  }
+}
